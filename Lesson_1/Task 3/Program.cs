0b1a73808a637a92f76ff28167ba96a418ccf6ec
Evgeniy Gorbachov <zekagor@gmail.com>
2023-07-11 13:15:21 +0300

    Task 3 is done.

---
+++ Lesson_1/Task 3/Program.cs
@@ -0,0 +1,21 @@
+﻿var listNumbers = new List<decimal>();
+
+for (var i = 0; i < 10; i++)
+{
+    Console.Write($"Enter {i + 1} number of array: ");
+    listNumbers.Add(Convert.ToDecimal(Console.ReadLine()));
+}
+
+Console.Write("Enter a number, which you want to dublicate: ");
+var number = Convert.ToDecimal(Console.ReadLine());
+
+for (var i = 0; i < 10; i++)
+{
+    if (listNumbers[i] == number)
+    {
+        listNumbers.Add(number);
+    }
+    
+}
+
+Console.WriteLine($"Your array after dublicating: {string.Join(", ", listNumbers)}");
