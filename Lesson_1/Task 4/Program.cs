ad29768ac124f7cf4ae2a1a4b5dfc7e661712196
Evgeniy Gorbachov <zekagor@gmail.com>
2023-07-11 13:41:08 +0300

    Task 4 with strings is done.

---
+++ Lesson_1/Task 4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter your first string: ");
+var string1 = Console.ReadLine();
+
+Console.Write("Enter your second string: ");
+var string2 = Console.ReadLine();
+
+if (string1.Length > string2.Length)
+{
+    Console.WriteLine(string1 + " " + string2);
+}
+else if (string1.Length == string2.Length)
+{
+}
+else
+{
+    Console.WriteLine(string2 + string1[0]);
+}
