c7d74c5a49480b2b4d70fa400c3cf9273145e142
Evgeniy Gorbachov <zekagor@gmail.com>
2023-07-15 02:13:47 +0300

    Was remade function with dublicating numbers, and now it dublicates numbers after previous same number.

--- Lesson_1/Task 3/Program.cs
+++ Lesson_1/Task 3/Program.cs
@@ -1,21 +1,37 @@
-﻿var listNumbers = new List<decimal>();
+﻿var listNumbers = new decimal[10];
 
 for (var i = 0; i < 10; i++)
 {
     Console.Write($"Enter {i + 1} number of array: ");
-    listNumbers.Add(Convert.ToDecimal(Console.ReadLine()));
+    listNumbers[i] = Convert.ToDecimal(Console.ReadLine());
+    // listNumbers.Add(Convert.ToDecimal(Console.ReadLine()));
 }
 
 Console.Write("Enter a number, which you want to dublicate: ");
 var number = Convert.ToDecimal(Console.ReadLine());
 
-for (var i = 0; i < 10; i++)
+for (var i = 0; i < listNumbers.Length; i++)
 {
     if (listNumbers[i] == number)
     {
-        listNumbers.Add(number);
+        var dublicatedNumberIndex = i + 1;
+        Array.Resize(ref listNumbers, listNumbers.Length + 1);
+        Array.Copy(listNumbers, i, listNumbers, dublicatedNumberIndex, listNumbers.Length - dublicatedNumberIndex);
+        listNumbers[dublicatedNumberIndex] = number;
+        i++;
     }
-    
 }
 
 Console.WriteLine($"Your array after dublicating: {string.Join(", ", listNumbers)}");
+
+// int[] myArray;
+// int indexToInsert = 2;
+// int elementToInsert = 10;
+//
+// Array.Resize(ref myArray, myArray.Length + 1); // increase array length by 1
+// Array.Copy(myArray, indexToInsert, myArray, indexToInsert + 1, myArray.Length - indexToInsert - 1); // shift elements right from the index
+// myArray[indexToInsert] = elementToInsert; // insert element at the index
+//
+// foreach(int num in myArray) {
+//     Console.WriteLine(num);
+// }
